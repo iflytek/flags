d017d13cbe6df2f0de524499be170e3a31d6b8ed
maybaby <ybyang7@iflytek.com>
2022-04-16 20:28:01 +0800

    feature: add log_helper

---
+++ nargs_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding:utf-8
+""" 
+@author: nivic ybyang7
+@license: Apache Licence 
+@file: nargs_test.py
+@time: 2022/04/10
+@contact: ybyang7@iflytek.com
+@site:  
+@software: PyCharm 
+
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛ 
+"""
+
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-sw', '--search_wav', nargs='+', default=['./content1', 'content2'], help="寻优用集合")
+
+args = parser.parse_args()
+
+print(args.search_wav)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'iflags',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Useful tools for writing script in AI engineering ',
     author='ybyang7',
     author_email='ybyang7@iflytek.com',
